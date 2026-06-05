79fecc2e46a287ce8f8783925b40bd4f2bc2ca69
Suman Karmakar <karmakarsuman12138@gmail.com>
2026-06-06 00:14:29 +0530

    fix: Update text and styles in Home page for Visitor Pass Management System

--- Fronted/src/pages/Home.jsx
+++ Fronted/src/pages/Home.jsx
@@ -2,24 +2,24 @@ const Home = () => {
   return (
     <div className="home">
       <div className="left">
-        <h1>A Visitor Pass Management System</h1>
+        <h1>A Visitor Pass Management System !</h1>
       </div>
       <div className="right">
         <div className="cart">
           <div className="cart-one">
             <p className="para">Visitor's apply for appointment</p>
-            <button className="btn">Visitor</button>
+            <button className="btn">Visitor's</button>
           </div>
           <div className="cart-two">
-            <p className="para">Help Visitors</p>
+            <p className="para">Help Visitor's</p>
             <button className="btn">Front Desk</button>
           </div>
           <div className="cart-three">
-            <p className="para">Give appointment to the visitors</p>
+            <p className="para">Give Appointment to the Visitor's</p>
             <button className="btn">Employee</button>
           </div>
           <div className="cart-four">
-            <p className="para">ALl the access & reports</p>
+            <p className="para">ALL the access & report's</p>
             <button className="btn">Admin</button>
           </div>
         </div>
